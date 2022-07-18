e4ba55f5e889616c1da89ea0bcfcdfc2b00baf06
Ahad <ahad15-12484@diu.edu.bd>
2022-07-18 10:11:11 +0600

    firestore navigation

--- app/src/main/java/com/ahad/firebasekotlin/firestore/FirestoreActivity.kt
+++ app/src/main/java/com/ahad/firebasekotlin/firestore/FirestoreActivity.kt
@@ -2,11 +2,21 @@ package com.ahad.firebasekotlin.firestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+
 import com.ahad.firebasekotlin.R
 
 class FirestoreActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_firestore)
+        setupActionBarWithNavController(findNavController(R.id.firestore_fragment))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.firestore_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
