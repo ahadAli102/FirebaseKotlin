27861a9e296de0243190e19f6fca25e2a5a2083b
Ahad <ahad15-12484@diu.edu.bd>
2022-07-19 19:52:06 +0600

    firestore display all product

--- app/src/main/java/com/ahad/firebasekotlin/firestore/HomeFragment.kt
+++ app/src/main/java/com/ahad/firebasekotlin/firestore/HomeFragment.kt
@@ -1,10 +1,12 @@
 package com.ahad.firebasekotlin.firestore
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
 import com.ahad.firebasekotlin.R
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -12,6 +14,7 @@ import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment(), ProductAdapter.ProductClickListener {
     private val adapter = ProductAdapter()
+    lateinit var viewModel: FireStoreViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,17 +27,37 @@ class HomeFragment : Fragment(), ProductAdapter.ProductClickListener {
         super.onViewCreated(view, savedInstanceState)
         recyclerview_products.adapter = adapter
         adapter.productClickListener = this
-        val products = mutableListOf<Product>()
-        products.add(
-            Product("123456","Sony Xperia Mark1",
-            "Sony","60Mp 4k camera, Snapdragon 888 SoC,4K LED display",1500.00f,5),
-        )
-        adapter.addProducts(products)
+//        val products = mutableListOf<Product>()
+//        products.add(
+//            Product("123456","Sony Xperia Mark1",
+//            "Sony","60Mp 4k camera, Snapdragon 888 SoC,4K LED display",1500.00f,5),
+//        )
+//        adapter.addProducts(products)
 
         add_product.setOnClickListener { view->
             val action = HomeFragmentDirections.actionHomeFragmentToAddFragment()
             Navigation.findNavController(view).navigate(action)
         }
+        viewModel = (activity as FirestoreActivity).viewModel
+
+        viewModel.readResponse.observe(viewLifecycleOwner, { response ->
+            when (response) {
+                is FireStoreResponse.Success -> {
+                    response.data?.let {
+                        Log.d(TAG, "onViewCreated: size ${response.data.size}")
+                        adapter.addProducts(response.data)
+                    }
+                }
+                is FireStoreResponse.Error -> {
+                    Toast.makeText(context, "Error occur: ${response.message}", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                is FireStoreResponse.Loading -> {
+                    Toast.makeText(context, "Reading Data", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        })
     }
 
     override fun onProductClicked(action: Product.Action, product: Product) {
